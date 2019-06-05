ce387c45c308bcebecd2a6405d8e5ea5affcfd7b
Plamena Radneva <rhythmxholic@gmail.com>
2019-06-05 11:28:27 +0300

    project base setup

---
+++ FetchThisApp/app/main.ts
@@ -0,0 +1,16 @@
+import Vue from 'nativescript-vue';
+import App from './components/App.vue';
+import store from './store';
+
+// Prints Vue logs when --env.production is *NOT* set while building
+Vue.config.silent = (TNS_ENV === 'production');
+
+Vue.registerElement(
+  'RadSideDrawer',
+  () => require('nativescript-ui-sidedrawer').RadSideDrawer
+)
+
+new Vue({
+store,
+  render: h => h('frame', [h(App)])
+}).$start();
