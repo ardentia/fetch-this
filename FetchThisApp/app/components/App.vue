40c06043c801dd5a7f6ce1aeafceb8c03f412a0f
Plamena Radneva <rhythmxholic@gmail.com>
2019-06-12 15:06:21 +0300

    add listview for shopping items, add sidedrawer and some fonticons

--- FetchThisApp/app/components/App.vue
+++ FetchThisApp/app/components/App.vue
@@ -1,33 +1,70 @@
 <template>
-    <Page>
-        <ActionBar>
-            <GridLayout width="100%" columns="auto, *">
-                <Label text="MENU" @tap="$refs.drawer.nativeView.showDrawer()" col="0"/>
-                <Label class="title" text="Welcome to NativeScript-Vue!"  col="1"/>
-            </GridLayout>
-        </ActionBar>
+  <Page>
+    <ActionBar>
+      <FlexboxLayout justifyContent="space-between" flexDirection="row" width="100%">
+        <Label class="fas"
+               width="10%"
+               :text="'fa-bars' | fonticon"
+               @tap="$refs.drawer.nativeView.showDrawer()"/>
 
-        <RadSideDrawer ref="drawer">
-            <StackLayout ~drawerContent backgroundColor="#ffffff">
-                <Label class="drawer-header" text="Drawer"/>
+        <Label class="title" width="75%" text="Fetch This!" />
 
-                <Label class="drawer-item" text="Item 1"/>
-                <Label class="drawer-item" text="Item 2"/>
-                <Label class="drawer-item" text="Item 3"/>
-            </StackLayout>
+        <Label class="fas" width="10%" :text="'fa-shopping-basket' | fonticon"/>
+      </FlexboxLayout>
+    </ActionBar>
 
-            <GridLayout ~mainContent columns="*" rows="*">
-                <Label class="message" :text="msg" col="0" row="0"/>
-            </GridLayout>
-        </RadSideDrawer>
-    </Page>
+    <RadSideDrawer ref="drawer">
+      <StackLayout ~drawerContent class="drawer-wrapper">
+        <Label class="drawer-header" text="Need More Actions?"/>
+
+        <Label class="drawer-item" text="View Completed Lists"/>
+        <Label class="drawer-item" text="Invite people"/>
+      </StackLayout>
+
+      <ScrollView ~mainContent>
+        <StackLayout class="home-panel">
+          <TextField v-model="textFieldValue" hint="Enter text..." />
+          <Button text="Add Item" @tap="onButtonTap" />
+
+          <ListView class="list-group" for="item in items" style="height:1250px">
+            <v-template>
+              <FlexboxLayout justifyContent="space-between" flexDirection="row" width="100%">
+                <CheckBox :checked="item.checked" :text="item.text" class="list-group-item-heading" width="90%"></CheckBox>
+                <Label class="fas" width="10%" :text="'fa-trash' | fonticon" :item="item" @tap="removeItemFromList" />
+              </FlexboxLayout>
+            </v-template>
+          </ListView>
+        </StackLayout>
+      </ScrollView>
+    </RadSideDrawer>
+  </Page>
 </template>
 
 <script lang="ts">
   export default {
     data() {
       return {
-        msg: 'Hello World!'
+        items: [],
+        textFieldValue: ''
+      }
+    },
+
+    methods: {
+      onButtonTap() {
+        if (this.textFieldValue &&!this.items.includes(this.textFieldValue)) {
+          this.items.push({
+            text: this.textFieldValue,
+            checked: false
+          });
+          this.textFieldValue = '';
+        }
+      },
+      removeItemFromList(args) {
+        const index = this.items.findIndex((item) => {
+          return item === args.object.item;
+        });
+
+        this.items.splice(index, 1);
       }
     }
   }
@@ -35,13 +72,24 @@
 
 <style scoped>
     ActionBar {
-        background-color: #53ba82;
+        background-color: #6C698D;
         color: #ffffff;
     }
 
     .title {
-        text-align: left;
-        padding-left: 16;
+      text-align: center;
+      font-size: 20;
+    }
+
+    .home-panel {
+      vertical-align: center;
+      font-size: 20;
+      margin: 15;
+      padding-top: 10;
+    }
+
+    .list-group {
+      margin: 5 0;
     }
 
     .message {
@@ -51,10 +99,14 @@
         color: #333333;
     }
 
+    .drawer-wrapper {
+      background-color: #D4D2D5;
+    }
+
     .drawer-header {
-        padding: 50 16 16 16;
+        padding: 20 16 16 16;
         margin-bottom: 16;
-        background-color: #53ba82;
+        background-color: #6C698D;
         color: #ffffff;
         font-size: 24;
     }
@@ -63,5 +115,11 @@
         padding: 8 16;
         color: #333333;
         font-size: 16;
+    }
+
+    FlexboxLayout .fas {
+      position: relative;
+      top: 2;
+      font-size: 30;
     }
 </style>
--- FetchThisApp/app/main.ts
+++ FetchThisApp/app/main.ts
@@ -1,6 +1,15 @@
 import Vue from 'nativescript-vue';
 import App from './components/App.vue';
 import store from './store';
+import { TNSFontIcon, fonticon } from 'nativescript-fonticon';
+
+TNSFontIcon.debug = true;
+TNSFontIcon.paths = {
+  'fa': './assets/fonts-css/fontawesome.css'
+};
+
+TNSFontIcon.loadCss();
+Vue.filter('fonticon', fonticon);
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = (TNS_ENV === 'production');
@@ -8,9 +17,14 @@ Vue.config.silent = (TNS_ENV === 'production');
 Vue.registerElement(
   'RadSideDrawer',
   () => require('nativescript-ui-sidedrawer').RadSideDrawer
-)
+);
+
+Vue.registerElement(
+  'CheckBox',
+  () => require('nativescript-checkbox').CheckBox
+);
 
 new Vue({
-store,
+  store,
   render: h => h('frame', [h(App)])
 }).$start();
